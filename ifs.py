c8e90ac9bf756b3553d9ebf305e781eca13d53e8
Howon Lee <hlee.howon@gmail.com>
2023-08-24 07:57:13 -0700

    thinking

--- ifs.py
+++ ifs.py
@@ -4,37 +4,41 @@ import functools
 
 import matplotlib.pyplot as plt
 
-def nn():
-    weights = [npr.randn(20, 20) / np.sqrt(20) for _ in range(100)]
-
-    def nonlinearity(res):
-        new_res = res.copy()
-        new_res[new_res < 0] *= 0.3
-        return new_res
+def nn(act_size):
+    weights = [npr.randn(act_size, act_size) / np.sqrt(act_size) for _ in range(30)]
 
     # def nonlinearity(res):
-    #     return res / res.sum()
+    #     new_res = res.copy()
+    #     new_res[new_res < 0] *= 0.3
+    #     return new_res
+
+    def nonlinearity(res):
+        return np.abs(res) / np.abs(res).sum()
 
     def closure_nn(inp):
         curr_res = inp
         for weight in weights:
             curr_res = nonlinearity(curr_res @ weight)
-        return curr_res.sum()
+        return curr_res[10]
     return closure_nn
 
-def res_img(f, size=50):
-    inp = npr.randn(20)
+def res_img(f, size=400, act_size=30):
+    inp = npr.randn(act_size)
     x = np.linspace(-5, 5, size)
     y = np.linspace(-5, 5, size)
     res = np.zeros((size, size))
+    print("total iters: ", size * size)
     for x_idx in range(size):
         for y_idx in range(size):
+            if ((x_idx * size) + y_idx) % 1000 == 0:
+                print("iter: ", ((x_idx * size) + y_idx), " / ", size * size)
             inp[0] = x[x_idx]
             inp[1] = y[y_idx]
             res[x_idx, y_idx] = f(inp)
     return res
 
 if __name__ == "__main__":
-    res = res_img(nn())
+    act_size = 30
+    res = res_img(nn(act_size), act_size=act_size)
     plt.imshow(res)
     plt.show()
---
+++ readme.md
@@ -0,0 +1,8 @@
+Iterated function system on random neural net
+
+- sample from last activation
+- damn it looks like turbulence
+ 
+- animation - time scale where its just evolution of the net?
+- velocity field from the animation... but note the time scale in the animation is pretty unintuitive
+- fourier spectra
